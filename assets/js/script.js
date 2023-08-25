c69125bf25e1a595b6d045bcca73184bddd7eeea
richie nolan <richienolan23@gmail.com>
2023-08-25 15:04:51 +0000

    add function to place invaders on game board with style

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ for (let i = 0; i < 342; i++) {
 
 const squares = Array.from(document.querySelectorAll('.game-area div'))
 
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    20,21,22,23,24,25,26,27,28,29,
+    40,41,42,43,44,45,46,47,48,49
+]
+
+function placeInvaders() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+}
+
+placeInvaders()
